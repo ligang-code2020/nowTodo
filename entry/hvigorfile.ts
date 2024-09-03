239f1ea69e7587091e0ea557959231bf2194c00c
ligang <ligang13874843373@gmail.com>
2024-09-03 17:20:26 +0800

    feat: 解决nav和库冲突

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -3,5 +3,5 @@ import { hapPlugin } from '@hadss/hmrouter-plugin';
 
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[hapPlugin]         /* Custom plugin to extend the functionality of Hvigor. */
+    plugins:[hapPlugin()]         /* Custom plugin to extend the functionality of Hvigor. */
 }
